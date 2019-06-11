89c6eb10963509aed1164dd4a3541af549d38be2
hendisantika <hendisantika@yahoo.co.id>
2019-06-11 14:27:27 +0700

    #14 - Add PersonData Entity

---
+++ src/main/java/com/hendisantika/personapi/entity/PersonData.java
@@ -0,0 +1,19 @@
+package com.hendisantika.personapi.entity;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-11
+ * Time: 14:27
+ */
+@Data
+public class PersonData {
+    private Results[] results;
+
+    private Info info;
+}
