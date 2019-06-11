365626a8c41300544256a0c640cd35e8568bad81
hendisantika <hendisantika@yahoo.co.id>
2019-06-11 14:19:34 +0700

    #6 - Add Timezone Entity

---
+++ src/main/java/com/hendisantika/personapi/entity/Timezone.java
@@ -0,0 +1,16 @@
+package com.hendisantika.personapi.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-11
+ * Time: 14:19
+ */
+public class Timezone {
+    private String offset;
+
+    private String description;
+}
