1342c558cb60c11f1bed3d387d87b48defc8f69d
hendisantika <hendisantika@yahoo.co.id>
2019-06-11 14:22:22 +0700

    #12 - Add Picture Entity

---
+++ src/main/java/com/hendisantika/personapi/entity/Picture.java
@@ -0,0 +1,22 @@
+package com.hendisantika.personapi.entity;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-11
+ * Time: 14:22
+ */
+@Data
+public class Picture {
+    private String thumbnail;
+
+    private String large;
+
+    private String medium;
+
+}
