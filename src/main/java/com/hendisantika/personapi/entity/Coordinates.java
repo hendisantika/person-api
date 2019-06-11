04014d721161952a949978a6aa3c5f581b7b38c3
hendisantika <hendisantika@yahoo.co.id>
2019-06-11 14:19:10 +0700

    #5 - Add Coordinates Entity

---
+++ src/main/java/com/hendisantika/personapi/entity/Coordinates.java
@@ -0,0 +1,19 @@
+package com.hendisantika.personapi.entity;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-11
+ * Time: 14:18
+ */
+@Data
+public class Coordinates {
+    private String latitude;
+
+    private String longitude;
+}
