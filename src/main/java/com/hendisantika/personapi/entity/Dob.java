253e9d5fba5a8bee8665971c671b8c5183d17e82
hendisantika <hendisantika@yahoo.co.id>
2019-06-11 14:21:02 +0700

    #9 - Add Dob Entity

---
+++ src/main/java/com/hendisantika/personapi/entity/Dob.java
@@ -0,0 +1,16 @@
+package com.hendisantika.personapi.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-11
+ * Time: 14:20
+ */
+public class Dob {
+    private String date;
+
+    private String age;
+}
