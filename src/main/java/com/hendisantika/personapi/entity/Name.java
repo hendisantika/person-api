4cacc81c5163e6654b2e21499139b7eaeeb85a36
hendisantika <hendisantika@yahoo.co.id>
2019-06-11 14:18:43 +0700

    #4 - Add Name Entity

---
+++ src/main/java/com/hendisantika/personapi/entity/Name.java
@@ -0,0 +1,21 @@
+package com.hendisantika.personapi.entity;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-11
+ * Time: 14:18
+ */
+@Data
+public class Name {
+    private String last;
+
+    private String title;
+
+    private String first;
+}
