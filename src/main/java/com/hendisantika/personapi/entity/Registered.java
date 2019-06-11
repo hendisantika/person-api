55983c1f5dd19673261f4115f602bfcab76110a3
hendisantika <hendisantika@yahoo.co.id>
2019-06-11 14:21:28 +0700

    #10 - Add Registered Entity

---
+++ src/main/java/com/hendisantika/personapi/entity/Registered.java
@@ -0,0 +1,19 @@
+package com.hendisantika.personapi.entity;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : person-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-11
+ * Time: 14:21
+ */
+@Data
+public class Registered {
+    private String date;
+
+    private String age;
+}
